8bba963257dd27dc71aeabe7bbc0d78d42d9a97b
Ahmed Zain Al-Hamawi <78156960+azain95@users.noreply.github.com>
2021-02-01 18:46:39 +0300

    your message here

---
+++ story.js
@@ -0,0 +1,4 @@
+let person = "Ahmad";
+let age = 25;
+let hobby = "Gaming";
+console.log (`Mr.${person}, In 33 years you will be ${ age + 33 } years old. By then you'll have decades of ${hobby} practice!`)
